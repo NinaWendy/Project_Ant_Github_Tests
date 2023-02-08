d8cd42cf18d00cb7d412ac4c40c6712a61e13e63
NinaWendy <busolowendy@gmail.com>
2023-02-08 16:33:15 +0300

    Add test

--- src/MyClassTest.java
+++ src/MyClassTest.java
@@ -1,12 +1,17 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class MyClassTest {
 
 	@Test
-	public void testMain() {
-		fail("Not yet implemented");
+	public void testMain()  throws Exception  {
+//		fail("Not yet implemented");
+		String output = "Hello, Java World!";
+	    assertEquals(output,"Hello, Java World!");
 	}
 
 }
